c438f3c445547819a2ffc296e9e9fdd35f3fb41b
Александр Храмков <aleksandr.khramkov@simbirsoft.com>
2025-02-04 12:19:11 +0400

    Создал класс FirstTest

---
+++ src/test/java/quru/qa/FirstTest.java
@@ -0,0 +1,11 @@
+package quru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+    @Test
+    public void firstTest() {
+        Assertions.assertTrue(false);
+    }
+}
