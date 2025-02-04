0f9161fefb2fcf73c5cea97959e406708c50b8e8
Александр Храмков <aleksandr.khramkov@simbirsoft.com>
2025-02-04 13:27:17 +0400

    добавил комментарий FirstTest

--- src/test/java/quru/qa/FirstTest.java
+++ src/test/java/quru/qa/FirstTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
+    //Комментарий 1
+    //Комментарий 2
+    //Комментарий 3
     @Test
     public void firstTest() {
         Assertions.assertTrue(false);
